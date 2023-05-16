9bffcc6a85d471bdad2d996855066395818541d2
AmirKallas <amir.kallas98@gmail.com>
2023-05-16 11:02:23 +0200

    second committ

--- creator.py
+++ creator.py
@@ -4,29 +4,42 @@ import datetime
 HRR_PATH = 'C:\\Users\\amirk\\Desktop\\filler_data\\DBC-Lab(HRR).xlsx'
 TRANSFERT_PATH = 'C:\\Users\\amirk\\Desktop\\filler_data\\DBC-Lab(Transfer).xlsx'
 
-hrrWorkbook = openpyxl.load_workbook(HRR_PATH, read_only=True)
-
+hrrWorkbook = openpyxl.load_workbook(HRR_PATH, read_only=True, data_only=True)
 candidatiHrr = hrrWorkbook['Candidati']
-cell = str(candidatiHrr.cell(row=2, column=21 ).value)
-today  = str(datetime.date.today())
-oggi = cell[0:10]
+
+# with open(HRR_PATH, 'r') as 
+
+TODAY  = str(datetime.date.today())
+stato = 'CV al Cliente'
+
 
 rows = candidatiHrr.max_row
 cols = candidatiHrr.max_column
 
-# print(type(today))
-# print(type(cell))
-# print(today)
-# print(oggi)
-# print(today == oggi)
-# print(rows)
-# print(cols)
-
+row_cells = [] #lista di righe utili
 for row_num in range(1, 5):
-    dataCell = candidatiHrr.cell(row=row_num, column=21).value
-    data = str(dataCell)[0:10]
-    if data == today:
-        print(dataCell)
+    if str(candidatiHrr.cell(row=row_num, column=21).value)[0:10] == TODAY and str(candidatiHrr.cell(row=row_num, column=20).value) == stato: #compara data odierna con data ria
+        tempList = []
+        for col_num in range(1, cols+1):
+            
+            value = candidatiHrr.cell(row=row_num, column=col_num).value
+            tempList.append(str(value))
+        row_cells.append(tempList)
 
-# dataCell = str(candidatiHrr.cell(row=2, column=21).value)[0:10]
-# print(dataCell)
+# print(', '.join(row_cells))
+print(row_cells)
+
+# lines_list = [row_cells for row_cells in range(0, len(row_cells)+1, 25)]
+
+# lines_list = []
+# for i in range(0, len(row_cells)+1):
+    
+
+
+# print(lines_list)
+
+# print(row_cells)
+
+
+
+
